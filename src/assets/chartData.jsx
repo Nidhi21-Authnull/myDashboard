04d77623955dcd3b44cf1cfaa291884483b71777
Nidhi Sharma <nidhisharma_cse21@ggct.co.in>
2024-10-06 19:31:27 +0530

    dashboard added

---
+++ src/assets/chartData.jsx
@@ -0,0 +1,70 @@
+export const dataLine = {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    datasets: [
+        {
+            label: 'Sales',
+            data: [65, 59, 80, 81, 56, 55, 60],
+            fill: false,
+            backgroundColor: 'rgba(75,192,192,0.4)',
+            borderColor: 'rgba(75,192,192,1)',
+        },
+    ],
+};
+
+export const dataBar = {
+    labels: ['Product A', 'Product B', 'Product C', 'Product D'],
+    datasets: [
+        {
+            label: 'Quantity',
+            data: [12, 19, 3, 5],
+            backgroundColor: 'rgba(153, 102, 255, 0.2)',
+            borderColor: 'rgba(153, 102, 255, 1)',
+            borderWidth: 1,
+        },
+    ],
+};
+export const dataPie = {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [
+        {
+            label: 'Colors Distribution',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)',
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)',
+            ],
+            borderWidth: 1,
+        },
+    ],
+};
+export const dataRadar = {
+    labels: ['Running', 'Swimming', 'Cycling', 'Basketball', 'Football', 'Tennis'],
+    datasets: [
+        {
+            label: 'Athlete A',
+            data: [20, 15, 25, 30, 35, 40],
+            backgroundColor: 'rgba(255, 99, 132, 0.2)',
+            borderColor: 'rgba(255, 99, 132, 1)',
+            borderWidth: 1,
+        },
+        {
+            label: 'Athlete B',
+            data: [25, 10, 20, 35, 40, 30],
+            backgroundColor: 'rgba(54, 162, 235, 0.2)',
+            borderColor: 'rgba(54, 162, 235, 1)',
+            borderWidth: 1,
+        },
+    ],
+};
